50acff0addeb979ef0b0209124708a916a44540b
KAmrutha <157336098+KAmrutha@users.noreply.github.com>
2024-05-01 13:49:36 -0400

    Update AddExpense.jsx

--- src/Components/AddExpense.jsx
+++ src/Components/AddExpense.jsx
@@ -188,7 +188,7 @@ export default function AddExpense() {
             <chakra.h3 fontSize="xl" fontWeight="bold" textAlign="left">
               Total Expenditure
             </chakra.h3>
-            <Button class="chakra-button css-19t6vfn" onClick={onOpen}>Add Expense</Button>
+            <Button onClick={onOpen}>Add Expense</Button>
           </Flex>
           <Divider />
           <TableContainer>
